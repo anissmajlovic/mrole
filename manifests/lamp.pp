772445b924b2a38003d9651a6f188349acfeac79
anissmajlovic <anis.smajlovic@sky.de>
2021-03-17 10:40:50 +0000

    first commit

---
+++ manifests/lamp.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include mrole::lamp
+class mrole::lamp {
+  include mprofile::base
+  include mprofile::apache
+  include mprofile::mysql::server
+}
